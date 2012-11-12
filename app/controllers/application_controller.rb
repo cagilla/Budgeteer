6615f67508bf055b25115b0a8e8bc94cdc8eb729
Chris Gillanders <cgillanders@confinedphoenix.com>
2012-11-12 06:31:41 -0500

    added code to application controller/helper to allow link back from page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,43 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  before_filter :link_return
+  
+  # FROM: http://ethilien.net/archives/better-redirects-in-rails/
+
+	def redirect_to_index(msg = nil)
+	  flash[:notice] = msg if msg
+	  redirect_to :action => 'index'
+	end
+	def flash_redirect(msg, *params)
+	  flash[:notice] = msg
+	  redirect_to(*params)
+	end
+
+    # redirect somewhere that will eventually return back to here
+	def redirect_away(*params)
+	  session[:original_uri] = request.request_uri
+	  redirect_to(*params)
+	end
+
+	# returns the person to either the original url from a redirect_away or to a default url
+	def redirect_back(*params)
+	  uri = session[:original_uri]
+	  session[:original_uri] = nil
+	  if uri
+	    redirect_to uri
+	  else
+	    redirect_to(*params)
+	  end
+	end
+
+private
+
+	# handles storing return links in the session
+	def link_return
+	  if params[:return_uri]
+	    session[:original_uri] = params[:return_uri]
+	  end
+	end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,16 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # def link_away(name, options = {}, html_options = nil)
+  #   link_to(name, { :return_uri => url_for(:only_path => true) }.update(options.symbolize_keys), html_options)
+  # end
+  def link_away(name, options = {}, html_options = nil)
+    link = case options
+        when String
+            link_to(name, options + "?return_uri=" + url_for(:only_path => true), html_options)
+        else
+            link_to(name, { :return_uri => url_for(:only_path => true) }.update(options.symbolize_keys), html_options)
+        end
+   end
 end
